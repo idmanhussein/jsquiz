b7523f62ee921790c37dea3e9bb28361824fc33d
Idman <idmanh95z@gmail.com>
2020-09-16 23:14:01 +0100

    used object literals for my questions and an array to hold them together

--- src/modules/questions.js
+++ src/modules/questions.js
@@ -1,11 +1,37 @@
 const questions = [
   {
-    question: "What is a verb?",
-    choice1: "A word that describes a noun",
-    choice2: "A word that describes an action",
-    choice3: "A naming word",
-    answer2: "a word that describes an action",
+    question:
+      "'He had a --- memory and he failed the memory test'. - Fill in the blank with one of the four options.",
+    answers: {
+      a: "Week",
+      b: "EKe",
+      c: "Woke",
+      d: "Weak",
+    },
+    correctAnswer: "a",
+  },
+
+  {
+    question:
+      "'The dress still looks --- even after two months of use.' - Fill in the blank with one of the four options.",
+    answers: {
+      a: "New",
+      b: "Knew",
+      c: "Gnu",
+      d: "Know",
+    },
+    correctAnswer: "b",
+  },
+
+  {
+    question:
+      "'The money he gave me helped me to --- over the difficult period.' - Fill in the blank with one of the four options.",
+    answers: {
+      a: "Tied",
+      b: "Tried",
+      c: "Tide",
+      d: "Tired",
+    },
+    correctAnswer: "c",
   },
 ];
-
-export default questions;
