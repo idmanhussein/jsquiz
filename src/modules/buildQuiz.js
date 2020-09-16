a6c29e3febec188f5b75aaa8f23a472b79b33088
Idman <idmanh95z@gmail.com>
2020-09-16 23:12:31 +0100

    function to display quiz questions. Used template literals to make questions easier to iterate over. Used forEach loop

--- src/modules/buildQuiz.js
+++ src/modules/buildQuiz.js
@@ -1,3 +1,22 @@
-function buildQuiz() {}
+function buildQuiz() {
+  const output = [];
 
-export default buildQuiz;
+  myQuestions.forEach((currentQuestion, questionNumber) => {
+    const answers = [];
+
+    for (letter in currentQuestion.answers) {
+      answers.push(
+        `<label>
+              <input type="radio" name="question${questionNumber}" value="${letter}">
+              ${letter} :
+              ${currentQuestion.answers[letter]}
+            </label>`
+      );
+    }
+    output.push(
+      `<div class="question"> ${currentQuestion.question} </div>
+          <div class="answers"> ${answers.join("")} </div>`
+    );
+  });
+  quizContainer.innerHTML = output.join("");
+}
