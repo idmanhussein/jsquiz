85fc836a6ef8ebdd7822c955a9bf2f1f51da92d6
Idman <idmanh95z@gmail.com>
2020-09-17 21:17:11 +0100

    final list of questions

--- src/modules/questions.js
+++ src/modules/questions.js
@@ -34,4 +34,88 @@ const questions = [
     },
     correctAnswer: "c",
   },
+
+  {
+    question:
+      "'Eat everything on the plate, you should not --- food. - Fill in the blank with one of the four options.'",
+    answers: {
+      a: "Waste",
+      b: "Waist",
+      c: "West",
+      d: "Vest",
+    },
+    correctAnswer: "a",
+  },
+
+  {
+    question:
+      "'He was asked to display his --- at the exhibition. - Fill in the blank with one of the four options.'",
+    answers: {
+      a: "Wear",
+      b: "Ware",
+      c: "Where",
+      d: "Were",
+    },
+    correctAnswer: "b",
+  },
+
+  {
+    question:
+      "'You should --- the clothes well so they will dry faster. - Fill in the blank with one of the four options.'",
+    answers: {
+      a: "Ping",
+      b: "Cringe",
+      c: "Ring",
+      d: "Wring",
+    },
+    correctAnswer: "d",
+  },
+
+  {
+    question:
+      "'This tree has a --- that is eight feet deep. - Fill in the blank with one of the four options.'",
+    answers: {
+      a: "Root",
+      b: "Route",
+      c: "Rate",
+      d: "Rite",
+    },
+    correctAnswer: "a",
+  },
+
+  {
+    question:
+      "'He needs to take a --- as he has been exercising for two hours. - Fill in the blank with one of the four options.'",
+    answers: {
+      a: "Broke",
+      b: "Bray",
+      c: "Brake",
+      d: "Break",
+    },
+    correctAnswer: "d",
+  },
+
+  {
+    question:
+      "'I have been wearing this --- of shoes for a long time. - Fill in the blank with one of the four options.'",
+    answers: {
+      a: "Pear",
+      b: "Pare",
+      c: "Pair",
+      d: "Payer",
+    },
+    correctAnswer: "c",
+  },
+
+  {
+    question:
+      "'The --- man delivered the letter today. - Fill in the blank with one of the four options.'",
+    answers: {
+      a: "Male",
+      b: "Mail",
+      c: "Melee",
+      d: "May",
+    },
+    correctAnswer: "b",
+  },
 ];
