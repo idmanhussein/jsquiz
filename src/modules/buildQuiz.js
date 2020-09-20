bc3761fe2a3027766ed1f507c446ea4322264980
Idman <idmanh95z@gmail.com>
2020-09-20 15:11:16 +0100

    gave 'letter' in for-in loop a variable. Gave input a class name of 'radio-button'

--- src/modules/buildQuiz.js
+++ src/modules/buildQuiz.js
@@ -1,14 +1,19 @@
-import { questions } from "./questions.js";
+import questions from "./questions.js";
+import showResults from "./showResults.js";
+
+const submitButton = document.getElementById("submit");
+const quizContainer = document.getElementById("quiz");
+
 function buildQuiz() {
   const output = [];
 
   questions.forEach((currentQuestion, questionNumber) => {
     const answers = [];
 
-    for (letter in currentQuestion.answers) {
+    for (const letter in currentQuestion.answers) {
       answers.push(
         `<label>
-          <input type="radio" name="question${questionNumber}" value="${letter}">
+          <input type="radio" class="radio-button" name="question${questionNumber}" value="${letter}">
           ${letter} :
           ${currentQuestion.answers[letter]}
         </label>`
@@ -21,6 +26,7 @@ function buildQuiz() {
   });
 
   quizContainer.innerHTML = output.join("");
+  submitButton.addEventListener("click", showResults(quizContainer));
 }
 
 export default buildQuiz;
